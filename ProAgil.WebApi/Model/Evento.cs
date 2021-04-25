fe4787602910795f25aa51a63d907c885989affa
mayconleles <maycon_feedbackari@outlook.com>
2021-04-25 13:59:12 -0300

    Inicial do Meu Projeto de Eventos

---
+++ ProAgil.WebApi/Model/Evento.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace Model
+{
+    public class Evento
+    {
+        [Key]
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+    }
+}
