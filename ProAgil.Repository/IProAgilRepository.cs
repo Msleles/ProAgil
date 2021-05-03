283c0763df9ee1d08b77d275ac3b3093d8077587
Msleles <lelesmaycon2021@outlook.com>
2021-05-03 15:04:07 -0300

    create interface repository

---
+++ ProAgil.Repository/IProAgilRepository.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using ProAgil.Domain;
+
+namespace ProAgil.Repository
+{
+    public interface IProAgilRepository
+    {
+        //GERAL
+        void add<T > (T entity) where T: class;
+        void Update< T > (T entity) where T: class;
+        void Delete< T > (T entity) where T: class;
+        Task<bool> SaveChangesAsync();
+
+        //EVENTOS
+        Task<Evento[]> GetAllEventosAsyncByTema(string tema , bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento> GetAllEventosAsyncById(int EventoId,bool includePalestrantes);
+        
+        //PALESTRANTE
+         Task<Evento[]> GetAllPalestrantesAsync(bool includePalestrantes);
+        Task<Evento> GetAllPalestranteAsyncById(int PalestranteId , bool includePalestrantes);
+
+       
+    }
+}
